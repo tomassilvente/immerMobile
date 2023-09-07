011f6a511de188b25ec7ddb6f4e64a1568b57eb2
martinagustian <martinagustia@yahoo.com>
2023-09-07 22:58:01 +0700

    add new branch

---
+++ src/app/event-discovery/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function EventDiscovery() {
+  return (
+    <>
+      <div>
+        <div>
+          <span></span>
+          <span>New York City, USA</span>
+        </div>
+        <div>
+          <span></span>
+          <span>Dec 15</span>
+        </div>
+      </div>
+      <div>
+        <span></span>
+        <input />
+      </div>
+      <div>
+        <div>
+          <div>Nearby Events (9)</div>
+          <div>See All</div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>Brooks Davis Live</div>
+          <div>15th Dec, 2023</div>
+          <div>
+            <span>10.00pm</span>
+            <span>$98</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>Browse by Category</div>
+        <div>
+          <img src="" alt="" />
+        </div>        
+      </div>
+      <div>
+        <div>Popular Events</div>
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <div>Brooks Davis Live</div>
+            <div>
+              <span></span>
+              <span>Sat, 15 Sept, 2023</span>
+            </div>
+            <div>
+              <span></span>
+              <span>6:25pm-10.00pm</span>
+            </div>
+            <div>Price: $98</div>
+          </div>
+          <div>
+            :
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EventDiscovery;
