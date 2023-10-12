e693ae43c473a13107542b8f0c64d005c2d9101b
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-10-12 16:32:19 +0300

    Onboarding screens

---
+++ src/app/onboardingScreens/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OnboardingPage from './components/OnboardingPage'
+
+const page = () => {
+  return (
+    <div>
+      <OnboardingPage />
+    </div>
+  )
+}
+
+export default page
