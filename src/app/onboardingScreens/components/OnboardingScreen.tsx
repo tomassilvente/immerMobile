d89f10076144f5b26c5e766a86fb9e87ad1e1466
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-10-20 14:33:33 +0300

    Improved responsiveness

--- src/app/onboardingScreens/components/OnboardingScreen.tsx
+++ src/app/onboardingScreens/components/OnboardingScreen.tsx
@@ -13,16 +13,18 @@ interface OnboardingScreenProps {
 
 const OnboardingScreen: React.FC<OnboardingScreenProps> = ({ title, description, pageNumber, totalPages, onSkipClick, onNext, backgroundImage, isLastScreen }) => {
     return (
-    <div className="h-screen w-screen flex flex-col justify-between bg-cover bg-center bg-no-repeat relative p-12" style={{ backgroundImage: `url('${backgroundImage}')` }}>
-      <div>
-        {pageNumber !== 0 && <button className="absolute text-white right-0 p-12 w-18 h-14" onClick={onSkipClick}>Skip</button>}
+    <div className="h-screen w-100 flex flex-col justify-between bg-cover bg-center bg-no-repeat px-5" style={{ backgroundImage: `url('${backgroundImage}')` }}>
+      <div className="w-100 flex justify-end">
+        {pageNumber !== 0 && <button className=" text-white p-12 h-14" onClick={onSkipClick}>Skip</button>}
       </div>
-      <div className='flex flex-col items-center'>
+      
+      <div className='w-100 flex flex-col items-center '>
         <div>
-          <h1 className="text-white text-3xl text-left font-bold mt-4">{title}</h1>
-          <p className="text-white text-left font-semibold mt-2">{description}</p>
+          <h1 className="text-white text-3xl leading-10 text-left font-bold mt-4">{title}</h1>
+          <p className="text-white text-xl text-left font-semibold mt-2">{description}</p>
         </div>
-        <div className='flex flex-col items-center'>
+
+        <div className=' flex flex-col items-center w-full md:w-[30%] lg:w-[30%]'>
           {pageNumber !== 0 && (
             <div className="flex items-center mt-8 mb-8">
               {[...Array(totalPages)].map((_, index) => (
@@ -33,8 +35,8 @@ const OnboardingScreen: React.FC<OnboardingScreenProps> = ({ title, description,
               ))}
             </div>
           )}
-          {!isLastScreen && <button className="bg-orange-500 text-white px-6 py-3 h-14 w-80 rounded-full mb-8" onClick={onNext}>Next</button>}
-          {isLastScreen && <button className="bg-orange-500 text-white px-6 py-3 h-14 w-80 rounded-full mb-8" onClick={onNext}>Get Started</button>}
+          {!isLastScreen && <button  className="w-[100%] bg-orange-500  text-white px-6 py-3 h-14  rounded-full mb-8" onClick={onNext}>Next</button>}
+          {isLastScreen && <button  className="w-[100%] bg-orange-500 text-white px-6 py-3 h-14  rounded-full mb-8" onClick={onNext}>Get Started</button>}
         </div>
         
       </div>
