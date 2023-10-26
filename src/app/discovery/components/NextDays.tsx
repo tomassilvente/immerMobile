89724ca1d10a0da2a12c3e84aa076c36202510ec
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-10-26 09:11:37 -0300

    [Architecture]: discovery page architecture fixed

--- src/app/discovery/components/NextDays.tsx
+++ src/app/discovery/components/NextDays.tsx
@@ -1,30 +1,35 @@
-"use client"
+"use client";
 import { useState } from "react";
 
-export default function NextDays({Days}:any){
-    const [isActive, setIsActive] = useState(false)
+interface Day {
+  day: string;
+  number: string;
+}
 
-    function setActive(){
-        setIsActive(!isActive)
-    }
-    return(
-        <div className="flex overflow-x-scroll mt-5 pb-4">
-                    {
-                        Days.map(day =>(
-                            <div    
-                                onClick={setActive}
-                                style={{
-                                    backgroundColor:
-                                    isActive
-                                    ? '#FFD6B8'
-                                    : '',
-                                }}
-                                className="border mr-3 shadow-lg hover:cursor-pointer rounded-md border-primary p-4 px-9 text-center text-lg">
-                                <p>{day.day}</p>
-                                <p className="text-sm">{day.number}</p>
-                            </div>
-                        ))
-                    }
-                </div>
-    )
+interface NextDaysProps {
+  Days: Day[];
+}
+
+export default function NextDays({ Days }: NextDaysProps) {
+  const [activeDayIndex, setActiveDayIndex] = useState<number | null>(null);
+
+  return (
+    <div className="flex overflow-x-scroll mt-5 pb-4">
+      {Days.map((day, index) => (
+        <div
+          onClick={() => setActiveDayIndex(index)}
+          className={`border mr-3 shadow-lg rounded-md border-primary p-4 px-9 text-center text-lg 
+                     ${
+                       activeDayIndex === index
+                         ? "bg-FFD6B8"
+                         : "hover:cursor-pointer"
+                     }`}
+          key={index}
+        >
+          <p>{day.day}</p>
+          <p className="text-sm">{day.number}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/app/discovery/page.tsx
+++ src/app/discovery/page.tsx
@@ -1,58 +1,81 @@
-import LargeCard from '../../components/LargeCard'
+import LargeCard from "../../components/LargeCard";
 import EventHeader from "../../components/EventHeader";
-import DemoData from '../../../public/data/DemoData.json'
+import DemoData from "../../../public/data/DemoData.json";
 import Link from "next/link";
 import SvgFilterIcon from "../../../public/assets/Icons/FilterIcon";
 
 import NextDays from "./components/NextDays";
-let Days=[
-    {
-        day: 'Sat',
-        number: '22 Oct'
-    },{
-        day: 'Sun',
-        number: '23 Oct'
-    },{
-        day: 'Mon',
-        number: '24 Oct'
-    },{
-        day: 'Tue',
-        number: '25 Oct'
-    },{
-        day: 'Wed',
-        number: '26 Oct'
-    },{
-        day: 'Thu',
-        number: '27 Oct'
-    },{
-        day: 'Fri',
-        number: '28 Oct'
-    },
-]
+let Days = [
+  {
+    day: "Sat",
+    number: "22 Oct",
+  },
+  {
+    day: "Sun",
+    number: "23 Oct",
+  },
+  {
+    day: "Mon",
+    number: "24 Oct",
+  },
+  {
+    day: "Tue",
+    number: "25 Oct",
+  },
+  {
+    day: "Wed",
+    number: "26 Oct",
+  },
+  {
+    day: "Thu",
+    number: "27 Oct",
+  },
+  {
+    day: "Fri",
+    number: "28 Oct",
+  },
+];
 
-export default function discovery(){
-
-    return(
-        <div className="h-screen bg-white text-black sm:invisible overflow-scroll">
-            <EventHeader eventImg={DemoData.eventImg}/>
-            <div className="m-5">
-                <p className="text-xl font-semibold mb-3">Event Details</p>
-                <div className="grid grid-cols-6">
-                    <Link href='/' className="text-primary col-start-1 col-end-4"> See Calendar </Link>
-                    <button className="border-primary shadow-sm col-start-6 flex border rounded-md">
-                        <p className="ml-3 mt-1 text-xs font-semibold">Filters</p>
-                        <SvgFilterIcon className="ml-3 mt-[3px]" height={20} width={20}/>
-                    </button>
-                </div>
-                <NextDays Days={Days} />
-                <div className="">
-                    {
-                        DemoData.cardData.map( DemoData=> (
-                            <LargeCard key={DemoData.title} eventImg={DemoData.image} eventOrganizer={DemoData.title} eventDay={DemoData.date} eventTime={DemoData.time} eventPrice={DemoData.price} liked={false}/>
-                        ))
-                    }
-                </div>
-            </div>
-        </div>
-    )
+interface CardDataProps {
+  title: string;
+  image: string;
+  date: string;
+  time: string;
+  price: number;
 }
+
+const Discovery: React.FC = () => {
+  return (
+    <div className="h-screen bg-white text-black sm:invisible overflow-scroll">
+      <EventHeader eventImg={DemoData.eventImg} />
+      <div className="m-5">
+        <p className="text-xl font-semibold mb-3">Event Details</p>
+        <div className="grid grid-cols-6">
+          <Link href="/">
+            <a className="text-primary col-start-1 col-end-4">See Calendar</a>
+          </Link>
+          <button className="border-primary shadow-sm col-start-6 flex border rounded-md">
+            <p className="ml-3 mt-1 text-xs font-semibold">Filters</p>
+            <SvgFilterIcon className="ml-3 mt-[3px]" height={20} width={20} />
+          </button>
+        </div>
+        <NextDays Days={Days} />
+        <div>
+          {DemoData.cardData.map((data: CardDataProps) => (
+            <LargeCard
+              key={data.title}
+              eventImg={data.image}
+              eventOrganizer={data.title}
+              eventDay={data.date}
+              eventTime={data.time}
+              eventPrice={data.price}
+              liked={false}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Discovery;
