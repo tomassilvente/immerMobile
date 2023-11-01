ab02ab2baba978451903757685c7da187c1551ac
Emmanuel Ayo <emmieaddiz@gmail.com>
2023-11-01 07:57:52 +0100

    [update]: update from pre-production.

--- src/app/ticket-cancellation/cancel/[id]/page.tsx
+++ src/app/ticket-cancellation/cancel/[id]/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { MobileLayout } from '../../../../components/MobileLayout';
 import CancelTicket from '../CancelTicket';
+import BackButton from '../../../../common/buttons/BackButton';
 
 export function generateStaticParams() {
     return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }]
@@ -17,12 +18,7 @@ const Page = ({params}: {params: {id: string}}) => {
         <section>
             <div className="relative flex px-[14px] gap-5 items-center py-5">
             <Link href='/ticket-cancellation/' className="action-button">
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
-                        <path d="M13 15.947L9 11.947L13 7.94702" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <rect x="0.5" y="1.44702" width="21" height="21" rx="9.5" stroke="#555555"/>
-                    </svg>
-                </span> 
+                <BackButton />
             </Link>
             <h1 className="font-bold text-lg">Cancel Ticket</h1>
             </div>
--- src/app/ticket-cancellation/page.tsx
+++ src/app/ticket-cancellation/page.tsx
@@ -5,6 +5,8 @@ import { MobileLayout } from '../../components/MobileLayout';
 import UpcomingTickets from './UpcomingTickets';
 import AttendedTickets from './AttendedTickets';
 import CancelledTickets from './CancelledTickets';
+import BackButton from '../../common/buttons/BackButton';
+import SearchButton from '../../common/buttons/SearchButton';
 
 const tabs = ['Upcoming', 'Attended', 'Cancelled'];
 
@@ -16,19 +18,11 @@ const Page = () => {
         <section>
         <div className="relative flex px-[14px] gap-5 items-center py-5">
             <Link href='' className="action-button">
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
-                        <path d="M13 15.947L9 11.947L13 7.94702" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <rect x="0.5" y="1.44702" width="21" height="21" rx="9.5" stroke="#555555"/>
-                    </svg>
-                </span> 
+                <BackButton />
             </Link>
             <h1 className="font-bold text-lg">Tickets</h1>
             <span className='ml-auto border border-[#555] p-[3px] rounded-lg'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
-            <path d="M6.91667 11.0833C9.494 11.0833 11.5833 8.994 11.5833 6.41667C11.5833 3.83934 9.494 1.75 6.91667 1.75C4.33934 1.75 2.25 3.83934 2.25 6.41667C2.25 8.994 4.33934 11.0833 6.91667 11.0833Z" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M12.7504 12.25L10.2129 9.71249" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+                <SearchButton />
             </span>
         </div>
         <div className='mt-5'>
