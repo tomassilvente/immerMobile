0a6a727d27d84a56e17f1d16c653061a0671955a
Faisal.yussuf <80989766+Wind-03@users.noreply.github.com>
2023-11-02 16:52:57 +0100

    Update Share.tsx

--- src/app/event-details/components/Share.tsx
+++ src/app/event-details/components/Share.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
-import link from "../../../../public/assets/link-2.svg"
-import telegram from '../../../../public/assets/telegram.svg'
-import whatsapp from '../../../../public/assets/whatsapp.svg'
-import twitter from '../../../../public/assets/twitter.svg'
-import instagram from '../../../../public/assets/instagram.svg'
-import search from '../../../../public/assets/search.svg'
+import search from '../../public/assets/search.svg'
 import data, { shareDataType } from './ShareData'
 import Send from './Send'
+import App from "./AppOptions";
 
 function contacts(SendData: shareDataType){
     return(
@@ -21,64 +17,7 @@ function Share(){
             <div className="w-[26px] h-[3px] rounded-[2.5px] bg-[#ABABAB] mx-auto my-[16px]"></div>
             <h1 className="text-black font-semibold text-xl pl-4 pt-5 pb-3">Share to</h1>
             <hr />
-            <div className="inline-flex justify-between py-[15px] items-center">
-                <div className="pl-[16px] flex flex-col items-center">
-                    <Image
-                        className="rounded-[20px] bg-[#ABABAB] p-[10px]"
-                        src={link}
-                        alt="copy link"
-                        width={46}
-                        height={46}
-                        priority
-                    />
-                    <p className="text-[#ABABAB] text-sm">Copy Link</p>
-                </div>
-                <div className="pl-[15px] flex flex-col items-center">
-                    <Image
-                        className="rounded-[20px] bg-gradient-to-t from-[#1FAF38] from-0% to-[#60D669] to-100% p-[10px]"
-                        src={whatsapp}
-                        alt="copy link"
-                        width={46}
-                        height={46}
-                        priority
-                    />
-                    <p className="text-[#ABABAB] text-sm">WhatsApp</p>
-                </div>
-                <div className="pl-[15px] flex flex-col items-center">
-                    <Image
-                        className="bg-black rounded-[20px] p-[10px]"
-                        src={twitter}
-                        alt="Twitter logo"
-                        width={46}
-                        height={46}
-                        priority
-                    />
-                    <p className="text-[#ABABAB] text-sm">Twitter</p>
-                </div>
-                <div className="pl-[15px] flex flex-col items-center">
-                    <Image
-                        className="rounded-[20px]"
-                        src={instagram}
-                        alt="Instagram Logo" 
-                        width={46}
-                        height={46}
-                        priority
-                    />
-                    <p className="text-[#ABABAB] text-sm">Instagram</p>
-                </div>
-                <div className="pl-[15px] flex flex-col items-center">
-                    <Image
-                        className="rounded-[20px]"
-                        src={telegram}
-                        alt="telegram logo"
-                        width={45}
-                        height={45}
-                        priority
-                    />
-                    <p className="text-[#ABABAB] text-sm">Telegram</p>
-                </div>
-                
-            </div>
+            <App />
             <hr/>
             <div className="border flex rounded-3xl h-[35px] m-5 mr-8 mt-8 mb-6 pl-[16px]">
                     <Image
