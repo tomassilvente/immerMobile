cae6970ad4b2c5b5d7f508c09adbd36b16aa831f
Apx-Monstr <apxmonstr@gmail.com>
2023-11-26 21:09:44 +0530

    Technical Support and assistance queries

---
+++ src/app/support/chat/components/Chats.tsx
@@ -0,0 +1,39 @@
+interface Data{
+    msg:string;
+    time:string;
+    byUser: boolean;
+}
+
+interface Chat{
+    chat:Data
+}
+
+interface Dataset{
+    dataset:Data[]
+}
+
+
+const Chat = ({chat}:Chat) =>{
+    return (
+        <div className="mb-7 mx-auto">
+            <div className={`p-4 w-fit max-w-[75%] ${chat.byUser ? "ml-auto rounded-l-lg rounded-tr-lg bg-primary text-white" : "bg-white text-black rounded-r-lg rounded-tl-lg"}`}>
+                {chat.msg}
+            </div>
+            <p className={`text-xs p-1 w-fit ${chat.byUser?"ml-auto":" "}`}>
+                {chat.time}
+            </p>
+        </div>
+    )
+}
+
+const Chats = ({dataset}:Dataset) =>{
+    return (
+        <div className="px-2.5 py-7">
+            {dataset.map((item, index) => (
+                <Chat key={index} chat={item} />
+            ))}
+        </div>
+    )
+}
+
+export default Chats;
---
+++ src/app/support/components/Submit.tsx
@@ -0,0 +1,9 @@
+const Submit = () =>{
+    return (
+        <div className="w-full text-white rounded-full bg-primary p-4 text-center my-2 cursor-pointer font-bold text-sm">
+            Send Comment
+        </div>
+    )
+}
+
+export default Submit;
