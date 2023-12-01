5ac342f5e108fe4561827110189e161d6a1cfc80
Tomas <silventetomas@gmail.com>
2023-12-01 12:57:21 -0300

    [ADD]: Podcast Rating Screen

--- src/app/content-discovery/podcast/components/About.tsx
+++ src/app/content-discovery/podcast/components/About.tsx
@@ -1,4 +1,7 @@
+"use client"
+import { useState } from "react"
 import SvgStar from "../../../../../public/assets/Icons/Star"
+import Image from "next/image"
 
 type AboutProps = {
     stars: number,
@@ -7,10 +10,18 @@ type AboutProps = {
 }
 
 export default function About({stars, category, about}:AboutProps){
+
+    const [isRateOpen, setIsRateOpen] = useState(false)
+
+    function rating(){
+        setIsRateOpen(!isRateOpen)
+    }
+
     return(
-        <>
-        <div className="flex m-4 ">
-            <div className={`border flex mr-3 rounded-full font-light p-1 pl-3 pr-4 text-center bg-primary text-white`}>
+        <div >
+        <div
+            className={`flex m-4`}>
+            <div onClick={rating}  className={`border flex mr-3 rounded-full font-light p-1 pl-3 pr-4 text-center bg-primary text-white`}>
                 <SvgStar /><p className="ml-1">{stars}</p>
             </div>
             <div className={`border flex-none mr-3 rounded-full font-light p-1 px-4 text-center bg-primary text-white`}>
@@ -18,6 +29,24 @@ export default function About({stars, category, about}:AboutProps){
             </div>
         </div>
         <p className="font-light text-sm m-5">{about}</p>
-        </>
+        {/* {isRateOpen && (
+        <div
+          onClick={rating}
+          className="fixed bottom-0 h-[100vh] left-0 right-0 top-0 w-[100%] z-10 flex items-center"
+        >
+           <Image src='../assets/Vert3.png' width={200} height={200} alt='...'/>
+        </div>
+      )} */}
+      {isRateOpen && (
+        <div
+          onClick={rating}
+          className="bg-[#ffffffd8] fixed bottom-0 h-[100vh] left-0 right-0 top-0 w-[100%] z-10 pt-[30%] pl-[20%]"
+        >
+          <Image className="h-[300px] " src='../assets/Vert3.png' width={300} height={400} alt='...'/>
+            <p className="text-3xl font-semibold w-[300px] text-center mt-3">How do you rate this podcast?</p>
+            <p className="text-primary text-4xl mt-5 w-[300px] text-center">★ ★ ★ ★ ★ </p>
+        </div>
+      )}
+        </div>
     )
 }
