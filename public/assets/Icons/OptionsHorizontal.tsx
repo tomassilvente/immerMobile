81d790b2dad7ccbc334f268ad628ab5fcca5fb3d
Tomas <silventetomas@gmail.com>
2023-12-15 16:27:44 -0300

    [FIX] Changes

--- public/assets/Icons/OptionsHorizontal.tsx
+++ public/assets/Icons/OptionsHorizontal.tsx
@@ -3,31 +3,32 @@ import type { SVGProps } from "react";
 const SvgOptionsHorizontal = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={20}
-    height={5}
+    width="1em"
+    height="1em"
     fill="none"
+    viewBox="0 0 23 23"
     {...props}
   >
     <circle
       cx={17.692}
-      cy={2.308}
-      r={2.308}
+      cy={1.8}
+      r={1.8}
       fill="#fff"
-      transform="rotate(90 17.692 2.308)"
+      transform="rotate(90 17.692 1.8)"
     />
     <circle
       cx={9.997}
-      cy={2.308}
-      r={2.308}
+      cy={1.8}
+      r={1.8}
       fill="#fff"
-      transform="rotate(90 9.997 2.308)"
+      transform="rotate(90 9.997 1.8)"
     />
     <circle
       cx={2.309}
-      cy={2.308}
-      r={2.308}
+      cy={1.8}
+      r={1.8}
       fill="#fff"
-      transform="rotate(90 2.31 2.308)"
+      transform="rotate(90 2.31 1.8)"
     />
   </svg>
 )
--- src/app/content-discovery/podcast/components/PodcastHeader.tsx
+++ src/app/content-discovery/podcast/components/PodcastHeader.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 //Icons
 import SvgMoreButton from "../../../../../public/assets/Icons/MoreButton"
 import SvgBackButton from "../../../../../public/assets/Icons/BackButton"
+import SvgOptionsHorizontal from "../../../../../public/assets/Icons/OptionsHorizontal"
+import SvgBackVectorWhite from "../../../../../public/assets/Icons/BackVectorWhite"
 
 
 export default function PodcastHeader({eventImg, eventTitle, listeners, category}: any){
@@ -13,8 +15,8 @@ export default function PodcastHeader({eventImg, eventTitle, listeners, category
                 backgroundPosition: "center",
                 backgroundSize: "cover",
             }}>
-                <Link href='/content-discovery' className="col-start-2 mr-7 "> <SvgBackButton height={40} width={40}/> </Link>
-                <Link href='/' className="col-start-12 mt-4 mr-2 "><SvgMoreButton width={35} height={35}/></Link>
+                <Link href='/content-discovery' className="col-start-2 mr-9"> <SvgBackVectorWhite height={52} width={30}/> </Link>
+                <Link href='/' className="col-start-12 mt-6 mr-4 "><SvgOptionsHorizontal width={35} height={40}/></Link>
                 <div className="col-start-1 col-end-6 mt-[220px]  text-white">
                     <p className="text-3xl">{eventTitle}</p>
                     <p className="text-sm">{listeners} listeners | {category}</p>
