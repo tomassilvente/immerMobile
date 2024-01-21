482945dc1aa0374844b292f9eef91494662f8eaf
Tomas <silventetomas@gmail.com>
2024-01-21 19:35:27 -0300

    Solving connectivity errors

--- src/server-actions/auth/googleLogin.ts
+++ src/server-actions/auth/googleLogin.ts
@@ -5,7 +5,8 @@ const googleLogin = async (): Promise<{ page:any }> => {
     method: 'GET',
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Access-Control': 'Allow-Origin'
     },
   })
 
